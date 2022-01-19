46a263084a3ab1a27fb38002965e031e9afdda10
InsanePhin <insanephin@gmail.com>
2022-01-19 14:12:08 +0900

    :fire: 다 만들었다

---
+++ index.py
@@ -0,0 +1,20 @@
+from pypresence import Presence
+import time, psutil
+
+client_id = "933214976817119263"
+rpc = Presence(client_id=client_id, pipe=0)
+rpc.connect()
+start_time = time.time()
+
+while True:
+    mem = psutil.virtual_memory()
+    rpc.update(
+        # buttons=[{"label": "Watchdog", "url": "https://petcafe.xyz/invite"}, {"label": "Server", "url": "https://discord.gg/jVc3ByWVd3"}], 
+        large_image="intel_pentium_2020_logo", large_text="Intel Pentium G3250T @ 2.80GHz",
+        small_image="microsoft-windows-server", small_text="Windows Server 2022 Datacenter", 
+        start=start_time, 
+        
+        details=f"CPUㅣ{round(psutil.cpu_percent(), 1)}%", 
+        state=f"RAMㅣ{round(mem.percent, 1)}%, {round(mem.used/1024**3, 1)}GB/{round(mem.total/1024**3, 1)}GB",
+    )
+    time.sleep(1)
